362803997f0a3107d07328fe03a2a22e5bf66b6c
Jinwon Jung <wlsdnjs16521@gmail.com>
2025-11-29 10:34:58 +0900

    feat: modify id type

--- apps/assassin/src/features/season/schemas/season.ts
+++ apps/assassin/src/features/season/schemas/season.ts
@@ -1,14 +1,14 @@
 import * as z from "zod";
 
 export const seasonSchema = z.object({
-  name: z.string(),
+  name: z.uuid(),
   description: z.string(),
   order: z.number().int(),
   isArchived: z.boolean().default(false),
 });
 
 export const seasonResponseSchema = seasonSchema.extend({
-  id: z.string(),
+  id: z.uuid(),
   createdAt: z.date(),
   updatedAt: z.date(),
 });
