828724e1f9b88cf9324089b2f7b421bdbda3858d
Jinwon Jung <wlsdnjs16521@gmail.com>
2025-12-02 22:28:25 +0900

    feat: update song schema and type

--- apps/assassin/src/features/song/schemas/song.ts
+++ apps/assassin/src/features/song/schemas/song.ts
@@ -1,22 +1,28 @@
 import * as z from "zod";
 
-export const songSchema = z.object({
+// DB에 저장할 때 입력 스키마
+export const createSongSchema = z.object({
   seasonId: z.uuid(),
-  name: z.string(),
-  artist: z.string(),
+  name: z.string().min(1, "Name required"),
+  artist: z.string().min(1, "Artist required"),
   description: z.string(),
   youtubeUrl: z.url(),
   order: z.number().int(),
-  writer: z.string(),
-  deletePw: z.string(),
+  writer: z.string().min(1, "Writer required"),
+  deletePw: z.string().min(1, "Password required"),
 });
 
-export const songResponseSchema = songSchema.extend({
+// 부분 업데이트
+export const updateSongSchema = createSongSchema.partial();
+
+// DB에서 받은 응답 스키마
+export const songSchema = createSongSchema.extend({
   id: z.uuid(),
   createdAt: z.date(),
   updatedAt: z.date(),
   deletedAt: z.date().nullable(),
 });
 
+export type CreateSongInput = z.infer<typeof createSongSchema>;
+export type UpdateSongInput = z.infer<typeof updateSongSchema>;
 export type Song = z.infer<typeof songSchema>;
-export type SongResponse = z.infer<typeof songResponseSchema>;
