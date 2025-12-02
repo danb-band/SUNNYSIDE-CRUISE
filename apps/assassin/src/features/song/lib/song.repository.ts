dd0afa7a665eeac0f29c2f5911925276054b55cd
Jinwon Jung <wlsdnjs16521@gmail.com>
2025-12-02 22:28:51 +0900

    feat: create song crud functions

---
+++ apps/assassin/src/features/song/lib/song.repository.ts
@@ -0,0 +1,70 @@
+import { supabase } from "@libs/supabase/client"
+import { CreateSongInput, songSchema, UpdateSongInput } from "../schemas/song"
+
+
+export async function getAllSongs() {
+  const { data, error } = await supabase
+    .from("song")
+    .select("*")
+    .order("createdAt", { ascending: false })
+
+  if (error) {
+    throw error
+  }
+
+  return songSchema.parse(data)
+}
+
+export async function getSongById(id: string) {
+  const { data, error } = await supabase
+    .from("song")
+    .select("*")
+    .eq("id", id)
+    .single()
+
+  if (error) {
+    throw error
+  }
+
+  return songSchema.parse(data)
+}
+
+export async function createSong(input: CreateSongInput) {
+  const { data, error } = await supabase
+    .from("song")
+    .insert([input])
+    .select()
+    .single()
+
+  if (error) {
+    throw error
+  }
+
+  return songSchema.parse(data)
+}
+
+export async function updateSong(id: string, input: UpdateSongInput) {
+  const { data, error } = await supabase
+    .from("song")
+    .update(input)
+    .eq("id", id)
+    .select()
+    .single()
+
+  if (error) {
+    throw error
+  }
+
+  return songSchema.parse(data)
+}
+
+export async function deleteSong(id: string) {
+  const { error } = await supabase
+    .from("song")
+    .delete()
+    .eq("id", id)
+
+  if (error) {
+    throw error
+  }
+}
