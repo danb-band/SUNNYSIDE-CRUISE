fa5fa4b9fd7e162d6e22056967b28ee8b691da0c
Jinwon Jung <wlsdnjs16521@gmail.com>
2025-12-02 22:34:36 +0900

    feat: update comment schema and type

--- apps/assassin/src/features/comment/schemas/comment.ts
+++ apps/assassin/src/features/comment/schemas/comment.ts
@@ -1,18 +1,24 @@
 import * as z from "zod";
 
-export const commentSchema = z.object({
+// DB에 저장할 때 입력 스키마
+export const createCommentSchema = z.object({
   songId: z.uuid(),
-  content: z.string(),
-  writer: z.string(),
-  deletePw: z.string(),
+  content: z.string().min(1, "Content required"),
+  writer: z.string().min(1, "Writer required"),
+  deletePw: z.string().min(1, "Password required"),
 });
 
-export const commentResponseSchema = commentSchema.extend({
+// 부분 업데이트
+export const updateCommentSchema = createCommentSchema.partial();
+
+// DB에서 받은 응답 스키마
+export const commentSchema = createCommentSchema.extend({
   id: z.uuid(),
   createdAt: z.date(),
   updatedAt: z.date(),
-  deletedAt: z.date(),
+  deletedAt: z.date().nullable(),
 });
 
+export type CreateCommentInput = z.infer<typeof createCommentSchema>;
+export type UpdateCommentInput = z.infer<typeof updateCommentSchema>;
 export type Comment = z.infer<typeof commentSchema>;
-export type CommentResponse = z.infer<typeof commentResponseSchema>;
