0fc81facceb4e0e15f0940aac63e9a5c22ede482
Jinwon Jung <wlsdnjs16521@gmail.com>
2025-12-14 21:16:29 +0900

    feat: create Comment crud server actions

---
+++ apps/assassin/src/features/comment/actions.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import CommentService from "./service";
+import { CommentPayload, CommentUpdatePayload } from "./schema";
+
+export const getCommentsAction = async () => {
+  return await CommentService.getAllComments();
+};
+
+export const getCommentAction = async (id: string) => {
+  return await CommentService.getCommentById(id);
+};
+
+export const getCommentsBySongAction = async (songId: string) => {
+  return await CommentService.getCommentsBySongId(songId);
+};
+
+export const createCommentAction = async (data: CommentPayload) => {
+  return await CommentService.createComment(data);
+};
+
+export const updateCommentAction = async (id: string, data: CommentUpdatePayload) => {
+  return await CommentService.updateComment(id, data);
+};
+
+export const deleteCommentAction = async (id: string) => {
+  return await CommentService.deleteComment(id);
+};
