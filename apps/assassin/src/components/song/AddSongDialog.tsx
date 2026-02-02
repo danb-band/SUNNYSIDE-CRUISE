2e7f819cacf1fbe14635dcb91e2fba0e837e6611
yerincho <forjustice9839@gmail.com>
2026-02-02 23:26:49 +0900

    refactor: set lint

--- apps/assassin/src/components/song/AddSongDialog.tsx
+++ apps/assassin/src/components/song/AddSongDialog.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect } from "react";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
