fcce4c6e1697e2b191bd6d356871dd1ed794b9d4
yerincho <forjustice9839@gmail.com>
2026-02-07 22:31:29 +0900

    ifix: add revalidation of path in season board cache logic

--- apps/assassin/src/libs/cache/seasonBoard.ts
+++ apps/assassin/src/libs/cache/seasonBoard.ts
@@ -1,7 +1,8 @@
-import { revalidateTag } from "next/cache";
+import { revalidatePath, revalidateTag } from "next/cache";
 
 export const SEASON_BOARD_CACHE_TAG = "board";
 
 export const revalidateSeasonBoard = () => {
+  revalidatePath("/");
   revalidateTag(SEASON_BOARD_CACHE_TAG, "max");
 };
