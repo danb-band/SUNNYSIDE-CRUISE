57d0f33b0e2f13c9027390bc5568c6cbe2d72f2e
yerincho <forjustice9839@gmail.com>
2026-02-11 00:34:46 +0900

    refactor: remove console.log

--- apps/assassin/src/components/song/SongDetailPage.tsx
+++ apps/assassin/src/components/song/SongDetailPage.tsx
@@ -3,11 +3,7 @@
 import { useRouter } from "next/navigation";
 import { SongDetailModal } from "./SongDetailModal";
 
-export function SongDetailPage({
-  songId,
-}: {
-  songId: string;
-}) {
+export function SongDetailPage({ songId }: { songId: string }) {
   const router = useRouter();
 
   return (
